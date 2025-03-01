166e2f6b5efe891c9b7c179ea155b308a9b0fa14
yeasin-islam <info.yeasinislam@gmail.com>
2025-03-01 23:11:36 +0600

    Commit for added discovary section and bsck to desk from discovary js function

--- script/discoverSomething.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("discoverSomething-btn").addEventListener("click",
-    function (event) {
-        event.preventDefault();
-        window.location.href="./main.html"
-    })
---
+++ utilities/utilities.js
@@ -0,0 +1,31 @@
+// alert("😒😒😒😒😒😒😒")
+
+//Function for converted a input value String to Float 
+function getInputValueByID(id) {
+    const value = document.getElementById(id).value;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+//Function for converted a inner text String to Float 
+function getInnerTextByID(id) {
+    const value = document.getElementById(id).innerText;
+    const convertedValue = parseFloat(value);
+    return convertedValue;
+}
+
+//Function for set a value in a inner text by id 
+function getInnerTextByIdAndValue(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+//Function for a form section block or none
+function handleToggle(id, status) {
+    document.getElementById(id).style.display = status;
+}
+
+// Get current date and time in a single formatted string
+function getCurrentDateTime() {
+    const now = new Date();
+    return now.toLocaleString(); // Format: MM/DD/YYYY, HH:MM AM/PM
+}
